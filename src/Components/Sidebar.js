a7f3252ac465523b0aefe407e49643f02d2f9b43
Muhammad Hasham <hasham25525@gmail.com>
2023-07-14 01:42:02 +0500

    Notebook

---
+++ src/Components/Sidebar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { PlusCircle } from 'lucide-react';
+import './sidebar.css'
+const Sidebar = () => {
+  const color=[]
+  return (
+    <div className='sidebar'>
+       <PlusCircle />
+       <ul className='sidebar_list'>
+        <li className='sidebar_list_item'></li>
+       </ul>
+    </div>
+  )
+}
+
+export default Sidebar
