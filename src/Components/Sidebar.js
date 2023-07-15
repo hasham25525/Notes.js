f1814e783d6b02cdd732994e1cbd9055dc43e76f
Muhammad Hasham <hasham25525@gmail.com>
2023-07-16 03:10:26 +0500

    sidebar added

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,16 +1,27 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { PlusCircle } from 'lucide-react';
 import './sidebar.css'
-const Sidebar = () => {
-  // const color=[]
+
+function Sidebar(props) {
+  const colors = ["#fe9b72", "#fec971", "#00d4fe", "#b693fd", "#e4ee91"];
+  const [listOpen, setListOpen] = useState(false);
   return (
-    <div className='sidebar pr-10 '>
-       <PlusCircle  />
-       <ul className='sidebar_list'>
-        <li className='sidebar_list_item'></li>
-       </ul>
+    <div className="sidebar">
+      <div onClick={() => setListOpen(!listOpen)} className="sidebar_add">
+        <PlusCircle />
+      </div>
+      <ul className={`sidebar_list pr-10 ${listOpen ? "sidebar_list_active" : ""}`}>
+        {colors.map((item, index) => (
+          <li
+            key={index}
+            style={{ backgroundColor: item }}
+            className="sidebar_list_item"
+            onClick={() => props.addNote(item)}
+          />
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
 
 export default Sidebar
